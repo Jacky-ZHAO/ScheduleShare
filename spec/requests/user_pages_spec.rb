9cffb69b58b503f992e431e38df98f8f93efedc0
ZHAO Liyao <zly0009@gmail.com>
2014-04-28 13:35:47 -0500

    finish delete function for admin

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,28 @@ describe "User pages" do
     it { should have_title('All users') }
     it { should have_content('All users') }
 
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
+
     describe "pagination" do
 
       before(:all) { 30.times { FactoryGirl.create(:user) } }
